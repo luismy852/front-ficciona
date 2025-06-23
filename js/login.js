666c6f214ddad79609aa7ab3aeb48f20054af17e
Luis Miguel <luismy852@gmail.com>
2025-06-22 22:05:08 -0500

    login y registro reponsive

--- js/login.js
+++ js/login.js
@@ -1,5 +1,78 @@
 import { API_URL } from "./config.js";
 
+document.addEventListener("DOMContentLoaded", function () {
+    const token = localStorage.getItem("token");
+
+    // Obtén los toggles y los menús móviles por separado
+    const menuToggleLogueado = document.getElementById("menuToggleLogueado");
+    const menuToggleAnonimo = document.getElementById("menuToggleAnonimo");
+
+    const menuMovilLogueado = document.getElementById("menuMovilLogueado");
+    const menuMovilAnonimo = document.getElementById("menuMovilAnonimo");
+
+    if (token) {
+        document.getElementById("headerLogueado").style.display = "flex";
+        document.getElementById("headerAnonimo").style.display = "none";
+
+        if (menuToggleLogueado && menuMovilLogueado) {
+            menuToggleLogueado.addEventListener("click", () => {
+                menuMovilLogueado.classList.toggle("activo");
+            });
+        }
+
+        const btnCerrar = document.getElementById("cerrarSesionBtn");
+        if (btnCerrar) {
+            btnCerrar.addEventListener("click", () => {
+                localStorage.removeItem("token");
+                localStorage.removeItem("idUsuario");
+                window.location.reload();
+            });
+        }
+
+    } else {
+        document.getElementById("headerLogueado").style.display = "none";
+        document.getElementById("headerAnonimo").style.display = "flex";
+
+        if (menuToggleAnonimo && menuMovilAnonimo) {
+            menuToggleAnonimo.addEventListener("click", () => {
+                menuMovilAnonimo.classList.toggle("activo");
+            });
+        }
+    }
+});
+
+
+
+//busqueda
+
+document.addEventListener("DOMContentLoaded", function () {
+    const campos = document.querySelectorAll(".textarea__header");
+    const botones = document.querySelectorAll(".boton__buscar");
+
+    // Suponiendo que cada campo tiene su botón correspondiente en el mismo orden
+    campos.forEach((campo, index) => {
+        const boton = botones[index];
+
+        if (boton) {
+            boton.addEventListener("click", function () {
+                const termino = campo.value.trim();
+                if (termino !== "") {
+                    window.location.href = `busqueda.html?query=${encodeURIComponent(termino)}`;
+                }
+            });
+
+            campo.addEventListener("keypress", function (e) {
+                if (e.key === "Enter") {
+                    e.preventDefault();
+                    boton.click();
+                }
+            });
+        }
+    });
+});
+
+
+
 document.getElementById("registroForm").addEventListener("submit", async function (event) {
     event.preventDefault(); // Evita que se recargue la página
 
